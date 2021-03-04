3c17357145f6b9b27f0e244a56223be7383f34e7
Oleh Pykulytsky <pykulytsky@gmail.com>
2021-03-04 20:25:23 +0200

    add test for login endpoint

---
+++ app/authentication/tests/test_login_api.py
@@ -0,0 +1,31 @@
+import pytest
+from authentication.models import User
+
+from django.urls import reverse
+
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_login_api_works(anon_api, performer_role):
+    user = User.objects.create_user(username='test', email='test@mail.com', password='1234', role=performer_role, first_name='test', last_name='test', is_active=True)
+    url = reverse('login')
+
+    response = anon_api.post(url, {
+        'email': user.email,
+        'password': '1234'
+    })
+
+    assert response.status_code == 200
+
+
+def test_login_api_get_token(anon_api, performer_role):
+    user = User.objects.create_user(username='test', email='test@mail.com', password='1234', role=performer_role, first_name='test', last_name='test', is_active=True)
+    url = reverse('login')
+
+    response = anon_api.post(url, {
+        'email': user.email,
+        'password': '1234'
+    })
+
+    assert response.data['token']
